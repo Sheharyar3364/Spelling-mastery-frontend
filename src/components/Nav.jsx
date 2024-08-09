886b0af327e9310395ef742cda3656854f071de2
Muhammad Sheharyar <muhammadsheharyar3364@gmail.com>
2024-08-09 15:24:19 +0500

    removed dark mode fully

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -35,7 +35,6 @@ export function Nav() {
           <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
               <span className="sr-only">Open user menu</span>
-              <img className="w-8 h-8 rounded-full" src="#" alt="user photo" />
             </button>
             <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow" id="user-dropdown">
               <div className="px-4 py-3">
